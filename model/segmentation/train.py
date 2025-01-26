40c1a1398dc6b0a7a416bc28c8abe2a0efa99083
Sacha Smart <sacha.smart@gmail.com>
2025-01-26 11:10:56 -0500

    results of first sgementation
    - not great at masking. Good at identifying

--- model/segmentation/train.py
+++ model/segmentation/train.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO, settings
 import torchvision
 import wandb
+from wandb.integration.ultralytics import add_wandb_callback
+
 
 
 settings.update({"wandb": True})
@@ -8,19 +10,21 @@ wandb.login()
 wandb.init(project="YOLO11n-segmentation", name="segmentation_pavement_cracks")
 
 model = YOLO("./yolo11n-seg.pt")
+add_wandb_callback(model, enable_model_checkpointing=True)
 
 results = model.train(
     data="data.segmentation.yaml",
     imgsz=640,
     batch=16,
     workers=8,
-    epochs=175,
+    epochs=150,
     device=0,
     project="runs/train",
     name="exp",
     exist_ok=True,
     cache=True,
     plots=True,
+    hyp=
 )
 
 wandb.finish()
